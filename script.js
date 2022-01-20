c0e7600d3e7229c1429414052e2e8b1e2c34f7f8
cyberkeiler <toby@terramultimedia.de>
2022-01-20 17:12:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 // Calling bootstrapExtra will initiliaze all the "custom properties"
 bootstrapExtra();
+
+WA.chat.sendChatMessage('Hello world', 'Mr Robot');
