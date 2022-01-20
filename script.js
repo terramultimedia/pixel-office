518d4c03028d23e451cb7c44aed4b633f77de241
cyberkeiler <tobias@sendelba.ch>
2022-01-20 20:00:50 +0100

    Test script

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 // Calling bootstrapExtra will initiliaze all the "custom properties"
 bootstrapExtra();
 
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+WA.chat.sendChatMessage("Hello world", "Mr Robot");
+WA.chat.onChatMessage((message) => {
+  WA.chat.sendChatMessage("Selbst, " + message + "!", "Mr Robot");
+  console.log("The user typed a message", message);
+});
